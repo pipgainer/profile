45050bc0148a16e6cb48e967f5fc4f6067fadb61
pipgainer <abhishekpowade@gmail.com>
2025-01-19 10:57:11 +0530

    animation for mobile screen

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const projectsData = [
     {
@@ -51,65 +51,86 @@ const projectsData = [
     }
 ];
 
-const ProjectCard = ({ date, title, description, demoLink, technologies, liveLink, githubLink }: any) => (
-    <div className="flex pointer-events-auto">
-        <div className="text-coolGray-400 bg-zinc-900 p-6 rounded-lg w-full flex flex-col transform transition lg:hover:scale-105 hover:opacity-100 lg:hover:shadow-2xl border-2 border-transparent hover:border-blue-500"        >
-            <section className="flex justify-between">
-                <span className="text-coolGray-500">{date}</span>
-            </section>
-            <h3 className="text-3xl font-semibold mt-8">{title}</h3>
-            <p className="text-coolGray-400 mt-4 mb-8">{description}</p>
-            <ul className="flex flex-wrap mt-auto mb-6 text-coolGray-500 text-sm font-semibold leading-6">
-                {technologies.map((tech: any, index: any) => (
-                    <li key={index}>
-                        {tech}
-                        {index < technologies.length - 1 && <span>&nbsp;•&nbsp;</span>}
-                    </li>
-                ))}
-            </ul>
-            <div className="flex justify-between text-green-400">
-                {(liveLink || demoLink) ? <a href={liveLink ?? demoLink} target="_blank" className="link" rel="noopener noreferrer">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18"
-                        height="18"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="inline-block mr-1.5 -mt-1"
-                    >
-                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 1 1 2-2h6"></path>
-                        <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                    </svg>
-                    {demoLink ? "Demo" : "Live"}
-                </a> : <div></div>}
-                {githubLink && <a href={githubLink} target="_blank" className="link" rel="noopener noreferrer">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="18"
-                        height="18"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="inline-block mr-1.5 -mt-1"
-                    >
-                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 1 1 2-2h6"></path>
-                        <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                    </svg>
-                    Github
-                </a>}
+const ProjectCard = ({ date, title, description, demoLink, technologies, liveLink, githubLink }: any) => {
+
+    const cardRef = useRef<HTMLDivElement>(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        if (window.innerWidth <= 640) {
+            const observer = new IntersectionObserver(
+                ([entry]) => setIsVisible(entry.isIntersecting),
+                { threshold: 1 }
+            );
+            if (cardRef.current) observer.observe(cardRef.current);
+            return () => observer.disconnect();
+        }
+    }, []);
+
+    return (
+        <div className="flex pointer-events-auto">
+            <div ref={cardRef}
+                className={`text-coolGray-400 bg-zinc-900 p-6 rounded-lg w-full flex flex-col transform transition ${isVisible ? "scale-105 opacity-100 shadow-2xl border-blue-500" : ""
+                    } lg:hover:scale-105 hover:opacity-100 lg:hover:shadow-2xl border-2 border-transparent hover:border-blue-500`}>
+                <section className="flex justify-between">
+                    <span className="text-coolGray-500">{date}</span>
+                </section>
+                <h3 className="text-3xl font-semibold mt-8">{title}</h3>
+                <p className="text-coolGray-400 mt-4 mb-8">{description}</p>
+                <ul className="flex flex-wrap mt-auto mb-6 text-coolGray-500 text-sm font-semibold leading-6">
+                    {technologies.map((tech: any, index: any) => (
+                        <li key={index}>
+                            {tech}
+                            {index < technologies.length - 1 && <span>&nbsp;•&nbsp;</span>}
+                        </li>
+                    ))}
+                </ul>
+                <div className="flex justify-between text-green-400">
+                    {(liveLink || demoLink) ? <a href={liveLink ?? demoLink} target="_blank" className="link" rel="noopener noreferrer">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="inline-block mr-1.5 -mt-1"
+                        >
+                            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 1 1 2-2h6"></path>
+                            <polyline points="15 3 21 3 21 9"></polyline>
+                            <line x1="10" y1="14" x2="21" y2="3"></line>
+                        </svg>
+                        {demoLink ? "Demo" : "Live"}
+                    </a> : <div></div>}
+                    {githubLink ? <a href={githubLink} target="_blank" className="link" rel="noopener noreferrer">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="18"
+                            height="18"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="inline-block mr-1.5 -mt-1"
+                        >
+                            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 1 1 2-2h6"></path>
+                            <polyline points="15 3 21 3 21 9"></polyline>
+                            <line x1="10" y1="14" x2="21" y2="3"></line>
+                        </svg>
+                        Github
+                    </a> : <div></div>}
+                </div>
             </div>
         </div>
-    </div>
-);
+        // </div>
+
+    )
+};
 
 const Projects = () => {
     return (
